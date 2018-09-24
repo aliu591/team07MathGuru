e309bdd5ebe7b51ac8211829f2b7f9fedad9abfc
Yue Pan <robertpanyue@gmail.com>
2018-09-24 19:29:55 -0400

    fix bugs

--- calculation2.java
+++ calculation2.java
@@ -1,11 +1,11 @@
 public class calculation2 {
-	public int function2(int n){
-		int res=1;
+	public double function2(int n){
+		double res=1;
 		int i = 1;
 		while(i<=n){
-		       res=i*res;
-		       i=i+1;
-		       }
+		       res = i * res;
+		       i = i + 1;
+		}
 		return res;
 	}
 }
