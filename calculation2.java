62900a54f276ab2d93826fc8f934b35c0acf2169
Yue Pan <robertpanyue@gmail.com>
2018-09-26 20:57:32 -0400

    Revert "Delete calculation2.java"
    This reverts commit b435632c1a7665973b6047ddb9c9ffce22bfbab0.

---
+++ calculation2.java
@@ -0,0 +1,11 @@
+public class calculation2 {
+	public double function2(int n){
+		double res=1;
+		int i = 1;
+		while(i<=n){
+		       res = i * res;
+		       i = i + 1;
+		}
+		return res;
+	}
+}
